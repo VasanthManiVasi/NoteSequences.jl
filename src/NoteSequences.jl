80b9c5d0dafa2e1652c046653d50f5a2c97b2cdc
Vasanth Mani Vasi <vasanth.mani.vasi.2@gmail.com>
2021-06-15 22:39:08 +0530

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/NoteSequences.jl
@@ -0,0 +1,5 @@
+module NoteSequences
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NoteSequences
+using Test
+
+@testset "NoteSequences.jl" begin
+    # Write your tests here.
+end
