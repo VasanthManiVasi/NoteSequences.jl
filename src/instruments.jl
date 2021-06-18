ed78a14e87717c8ae48fabc10ea97b46fb4d4d24
Vasanth Mani Vasi <vasanth.mani.vasi.2@gmail.com>
2021-06-19 01:14:39 +0530

    Improve getinstruments()

--- src/instruments.jl
+++ src/instruments.jl
@@ -1,6 +1,7 @@
 export getinstruments
 
-using MIDI: NoteOnEvent, NoteOffEvent, ProgramChangeEvent, ControlChangeEvent, PitchBendEvent, toabsolutetime!
+using MIDI: NoteOnEvent, NoteOffEvent, ProgramChangeEvent, ControlChangeEvent, PitchBendEvent
+using MIDI: toabsolutetime!, channelnumber
 using DataStructures: OrderedDict
 
 mutable struct Instrument
@@ -10,8 +11,23 @@ mutable struct Instrument
     pitch_bends::Vector{PitchBendEvent}
 end
 
-function getinstruments(midi_::MIDIFile)
-    MIDI.toabsolutetime!(midi_)
+function Instrument(program::Int)
+    Instrument(Notes(), program, Vector{ControlChangeEvent}(), Vector{ProgramChangeEvent}())
+end
+
+function toabsolutetime!(midi::MIDIFile)
+    for track in midi.tracks
+        toabsolutetime!(track)
+    end
+end
+
+function channel(event::MIDIEvent)
+    Int(channelnumber(event))
+end
+
+function getinstruments(midi::MIDIFile)
+    # TODO: Work on a copy of the midi since this function modifies it to abs time
+    toabsolutetime!(midi)
 
     # Create an instrument map which maps:
     # (program, channel, track) => Instrument
@@ -20,32 +36,39 @@ function getinstruments(midi_::MIDIFile)
     for (track_num, track) in enumerate(midi.tracks)
         # Create a map of channel => program to map the channels to the current playing instrument
         # Initially all channels map to the default program (0)
-        current_instrument = zeros(16)
+        current_instrument = zeros(Int, 16)
 
         # Create a map of (channel, note) => (NoteOn position, velocity)
-        note_map = Dict{Tuple{Int, Int}, Tuple{Int, Int}}()
+        note_map = Dict{Tuple{Int, Int}, Vector{Tuple{Int, Int}}}()
 
-        for event in track
+        for (idx, event) in enumerate(track.events)
             if event isa NoteOnEvent && event.velocity > 0
-                note_map[(event.channel, event.note)] = (event.dT, event.velocity)
+                key = (channel(event), event.note)
+                if haskey(note_map, key) 
+                    push!(note_map[key], (event.dT, event.velocity))
+                else
+                    note_map[key] = [(event.dT, event.velocity)]
+                end
             elseif event isa NoteOffEvent || (event isa NoteOnEvent && event.velocity == 0)
-                k = (event.channel, event.note)
-                if haskey(k, note_map)
+                k = (channel(event), event.note)
+                if haskey(note_map, k)
                     end_pos = event.dT
                     notes_playing = note_map[k]
                     turnoff_notes = [(pos, vel) for (pos, vel) in notes_playing if pos != end_pos]
                     continue_notes = [(pos, vel) for (pos, vel) in notes_playing if pos == end_pos]
 
-                    program = current_instrument[event.channel]
-                    key = (program, event.channel, track_num)
-                    if !haskey(key, instrument_map)
+                    program = current_instrument[channel(event) + 1]
+                    key = (program, channel(event), track_num)
+                    if !haskey(instrument_map, key)
                         # Create an instrument with the default program number
-                        instrument = instrument_map[(program, event.channel, track_num)]
+                        instrument = Instrument(program)
+                        instrument_map[(program, channel(event), track_num)] = instrument
                     else
                         instrument = instrument_map[key]
                     end
+
                     for (position, velocity) in turnoff_notes
-                        note = Note(event.note, velocity, position, position + end_pos, event.channel)
+                        note = Note(event.note, velocity, position, end_pos - position, channel(event))
                         push!(instrument.notes, note)
                     end
 
@@ -56,24 +79,25 @@ function getinstruments(midi_::MIDIFile)
                     end
                 end
             elseif event isa ProgramChangeEvent
-                current_instrument[event.channel] = event.program
+                current_instrument[channel(event) + 1] = event.program
                 # If the intrument map has an instrument at the default program with no notes,
                 # Change it's program to the current program
-                key = (0, event.channel, track_num)
-                if haskey(key, instrument_map)
+                key = (0, channel(event), track_num)
+                if haskey(instrument_map, key) && event.program != 0
                     instrument = instrument_map[key]
                     if isempty(instrument.notes)
                         instrument.program = event.program
-                        instrument_map[(event.program, event.channel, track_num)] = instrument
+                        instrument_map[(event.program, channel(event), track_num)] = instrument
                         delete!(instrument_map, key)
                     end
                 end
             elseif typeof(event) in [ControlChangeEvent, PitchBendEvent]
-                program = current_instrument[event.channel]
-                key = (program, event.channel, track_num)
-                if !haskey(key, instrument_map)
+                program = current_instrument[channel(event) + 1]
+                key = (program, channel(event), track_num)
+                if !haskey(instrument_map, key)
                     # Create an instrument with the default program number
-                    instrument = instrument_map[(0, event.channel, track_num)]
+                    instrument = Instrument(program)
+                    instrument_map[(0, channel(event), track_num)] = instrument
                 else
                     instrument = instrument_map[key]
                 end
